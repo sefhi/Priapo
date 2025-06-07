db703695573014b7bcc80cf537a706932b7a1939
jmalias <jm.alias@covermanager.com>
2025-06-07 10:38:14 +0200

    **feat: add index to work entry user_id for improved query performance**

---
+++ migrations/Version20250607083731.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250607083731 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add index to work entries column user_id.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE INDEX idx_work_entry_user_id ON app_work_entry (user_id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX idx_work_entry_user_id
+        SQL);
+    }
+}
