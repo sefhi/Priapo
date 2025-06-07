de3027e434106d9e3f43070828e4ade6e1b266a4
jmalias <jm.alias@covermanager.com>
2025-06-07 11:18:56 +0200

    feat: update end_date column in work entries to allow null values
    - Add migration to drop NOT NULL constraint from `end_date` column in `app_work_entry` table.
    - Provide a corresponding `down` method to revert the change.

---
+++ migrations/Version20250607091808.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250607091808 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add not null to end_date column in work entries.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE app_work_entry ALTER end_date DROP NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE app_work_entry ALTER end_date SET NOT NULL
+        SQL);
+    }
+}
